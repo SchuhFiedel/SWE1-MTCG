211cbf8f6045ad4fff64effe04dad9267078a481
Maximiliano Zulli <maximiliano.zulli@gmail.com>
2020-11-27 18:02:29 +0100

    Alles Auf Postgre Umgestellt!

--- MTCG/MTCG/Program.cs
+++ MTCG/MTCG/Program.cs
@@ -11,35 +11,19 @@ namespace MTCG
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            /*
-            Card card = new Hurricane();
-            Card card2 = new Hransig();
-            */
+            List<Card> cards = new List<Card>();
 
-            MySqlDataClass db = new MySqlDataClass();
-            List<Card> cardList = new List<Card>();
-            cardList = db.getCardsFromDB();
-            Console.WriteLine();
+            PostgreSqlClass db = new PostgreSqlClass();
 
-            for (int i = 0; i < cardList.Count; i++)
-            {
-                Console.WriteLine(cardList[i].GetCardName() + " |Card Type: " + cardList[i].GetCardType() + " |Piercing Damage: " + cardList[i].GetPiercing());
-                Console.WriteLine();
+            //db.Insert("INSERT INTO usertable (firstname, givenname, cardid) VALUES ((SELECT firstname from usertable where i_uid = 3), 'B' , 4);");
+            //Console.WriteLine("Insert Done");
+
+            cards = db.GetCardsFromDB();
+
+            foreach (Card x in cards){
+                Console.WriteLine(x.GetCardName());
             }
-
-            Console.WriteLine("Hransig HP : " + cardList[1].GetHP());
-            cardList[0].Attack(cardList[1]);
-            Console.WriteLine("Hransig HP : " + cardList[1].GetHP());
-
-
-            /*
-            MySqlDataClass db = new MySqlDataClass();
-            string queryString = "";
-            while (queryString != ".") {
-                queryString = Console.ReadLine();
-                if (queryString != ".") { db.runQuery(queryString); };
-            }
-            */
+            
             Console.ReadKey();
             
         }
