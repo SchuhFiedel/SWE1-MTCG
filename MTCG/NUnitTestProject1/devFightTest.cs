462f8ce5d9258f9b39f1193fa7a975fc28e4df09
Maximiliano Zulli <maximiliano.zulli@gmail.com>
2020-12-14 08:59:21 +0100

    Name change for Tests

--- MTCG/NUnitTestProject1/devFightTest.cs
+++ MTCG/NUnitTestProject1/devFightTest.cs
@@ -22,30 +22,6 @@ namespace Test
             cardList = db.getAllCardsFromDB();
         }
 
-        /*
-        [Test]
-        public void Test1()
-        {
-            CalcTestClass calc = new CalcTestClass();
-            int value1 = 17;
-            int value2 = 25;
-
-            int actualValue = calc.xPlusY(value1, value2);
-            int expectedVal = 17+25;
-
-            Assert.AreEqual(expectedVal, actualValue);
-        }
-
-        [Test]
-        public void HurricaneTest()
-        {
-            Card hur1 = new Hurricane();
-            string name = "Hurricane";
-            string actual = hur1.GetCardName();
-            Assert.AreEqual(name, actual);
-        }
-        */
-
 
         [Test]
         public void AttackTest_Hurricane_Hransig()
